1eac93bcd14d9fc4be268e1f352c4c219e76f752
LiuYongchao <1215266098@qq.com>
2023-12-20 22:38:44 +0800

    外观模式学习

---
+++ Assets/DesignPatterns/Script/FacadePattern/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace DesignPatterns.Script.FacadePattern
+{
+    /// <summary>
+    /// 敌人
+    /// </summary>
+    public class Enemy
+    {
+        /// <summary>
+        /// 寻找目标
+        /// </summary>
+        public void FindTarget()
+        {
+            Debug.Log("敌人寻找目标");
+        }
+
+        /// <summary>
+        /// 准备
+        /// </summary>
+        public void Preparation()
+        {
+            Debug.Log("敌人准备");
+        }
+
+        /// <summary>
+        /// 攻击目标
+        /// </summary>
+        public void AttackTarget()
+        {
+            Debug.Log("敌人攻击");
+        }
+
+        /// <summary>
+        /// 记录目标
+        /// </summary>
+        public void RecordObjectives()
+        {
+            Debug.Log("敌人记录");
+        }
+
+        /// <summary>
+        /// 攻击流程
+        /// </summary>
+        public void AttackCourse()
+        {
+            FindTarget();
+            Preparation();
+            AttackTarget();
+            RecordObjectives();
+        }
+
+        /// <summary>
+        /// 只记录信息的流程
+        /// </summary>
+        public void OnlyRecordCourse()
+        {
+        }
+    }
+}
---
+++ Assets/DesignPatterns/Script/FacadePattern/FacadePatternController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace DesignPatterns.Script.FacadePattern
+{
+    /// <summary>
+    /// 外观模式
+    /// 可以将较为复杂的流程封装起来，只给外部提供一个调用接口，逻辑清晰，方便调用和后续改动
+    /// </summary>
+    public class FacadePatternController : MonoBehaviour
+    {
+        private Enemy _enemy;
+
+        private void Awake()
+        {
+            _enemy = new Enemy();
+        }
+
+        private void Start()
+        {
+            //不使用外观模式，需要把Enemy内部的方法按照一定顺序调用，导致外部需要较多了解内部实现，不方便调用也不方便内部改动
+            if (CanAttack())
+            {
+                //如果可以攻击
+                _enemy.FindTarget();
+                _enemy.Preparation();
+                _enemy.AttackTarget();
+                _enemy.RecordObjectives();
+            }
+            else
+            {
+                //如果不能攻击
+                _enemy.FindTarget();
+                _enemy.RecordObjectives();
+            }
+
+            //使用外观模式以后，只要调用对应流程，不需要关注内部实现
+            if (CanAttack())
+            {
+                //如果可以攻击
+                _enemy.AttackCourse();
+            }
+            else
+            {
+                //如果不能攻击
+                _enemy.OnlyRecordCourse();
+            }
+        }
+
+        /// <summary>
+        /// 判断是否可以攻击
+        /// </summary>
+        /// <returns></returns>
+        private bool CanAttack()
+        {
+            return Random.Range(0, 5) < 3;
+        }
+    }
+}
